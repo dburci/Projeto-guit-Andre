2b7f4a4f3b76c1ad0b74e3894b26f61e0ad22458
Deborah <dburci@gmail.com>
2026-05-28 12:07:27 -0300

    Meu primeiro commit

---
+++ pessoas.py
@@ -0,0 +1,13 @@
+pessoas = {
+    "jonny": {"idade": 20 ,"aniversario": "12/03","altura": "1.80"},
+    "Pedro": {"idade": 30,"aniversario": "15/03"},
+    "José": {"idade": 24,"aniversario": "20/03"},
+    "Carol": {"idade": 32,"aniversario": "30/03"},
+    "Paulo": {"idade": 44,"aniversario": "10/03"},
+    "Cintia": {"idade": 19,"aniversario": "15/03"},
+    "Fernando": {"idade": 50,"aniversario": "15/03"},
+    "Valentina": {"idade": 22,"aniversario": "15/03"},
+    "Vladmir": {"idade": 80,"aniversario": "15/03"},
+    "Jessica": {"idade": 30,"aniversario": "15/03"},
+    "Lucas": {"idade": 18,"aniversario": "15/03"}
+}
